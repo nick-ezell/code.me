67bcf6045262f045ab333a48591aedb9f2fa29ad
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-02-08 12:09:43 -0600

    Compiled question arrays and wrote onclick event functions for each difficulty button, as well as corrected path for script file link.

--- script.js
+++ script.js
@@ -4,56 +4,60 @@ $(document).ready(function () {
             options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
         }
     });
-    
-    let easyArr = ["multiply", "even-or-odd", "opposite-number", 
-        "sum-of-positive", "find-the-smallest-integer-in-the-array", "string-repeat",
-         "return-negative", "remove-first-and-last-character", "remove-string-spaces", "reversed-strings",
+
+    let easyArr = ["multiply", "opposite-number",
+        "sum-of-positive", "find-the-smallest-integer-in-the-array", "remove-first-and-last-character", "remove-string-spaces",
         "vowel-count", "disemvowel-trolls"];
 
-    let mediumArr = ["get-the-middle-character", ""];
+    let mediumArr = ["get-the-middle-character", "century-from-year", "convert-a-number-to-a-string", "reversed-strings", "grasshopper-order-of-operations", "exes-and-ohs",
+        "shortest-word", "descending-order", "sum-of-odd-numbers", ""];
 
-    let hardArr = [];
+    let hardArr = ["create-phone-number", "who-likes-it", "find-the-odd-int", "counting-duplicates", "replace-with-alphabet-position", "convert-string-to-camel-case"];
 
 
     let easy = $("#easy")
     let medium = $("#medium");
     let hard = $("#hard");
 
-    function getChallenge(){
-            if(easy.prop("clicked") === true){
-                challengeIndex = Math.floor(Math.random() * easyArr.length)
-                challengeID = easyArr[challengeIndex];
-                
-            } else if(medium.prop("clicked") === true){
-                challengeIndex = Math.floor(Math.random() * mediumArr.length)
-                challengeID = mediumArr[challengeIndex];
-                
-            } else if(hard.prop("clicked") === true){
-                challengeIndex = Math.floor(Math.random() * hardArr.length)
-                challengeID = hardArr[challengeIndex];
-                
-            }
-            $.ajax({
-                url: "https://www.codewars.com/api/v1/code-challenges/" + challengeID,
-                headers: { "Authorization": "MhherAbCWBLgq-YZvg_G" },
-                method: "GET"
-            }).then(function (data) {
-                q.text(data.description)
-                console.log(data)
-            })
-        }
+    let q = $("#questionText");
 
+    easy.on("click", function () {
+        challengeIndex = Math.floor(Math.random() * easyArr.length)
+        challengeID = easyArr[challengeIndex];
+        $.ajax({
+            url: "https://www.codewars.com/api/v1/code-challenges/" + challengeID,
+            headers: { "Authorization": "MhherAbCWBLgq-YZvg_G" },
+            method: "GET"
+        }).then(function (data) {
+            q.text(data.description)
+            console.log(data)
+        })
+    })
 
-    let q = $("questionText");
-    
-    easy.click(function (){
-        getChallenge();
+    medium.on("click", function (){
+        challengeIndex = Math.floor(Math.random() * mediumArr.length)
+        challengeID = mediumArr[challengeIndex];
+        $.ajax({
+            url: "https://www.codewars.com/api/v1/code-challenges/" + challengeID,
+            headers: { "Authorization": "MhherAbCWBLgq-YZvg_G" },
+            method: "GET"
+        }).then(function (data) {
+            q.text(data.description)
+            console.log(data)
+        })
     })
-    medium.click(function (){
-        getChallenge();
-    })
-    hard.click(function (){
-        getChallenge();
+
+    hard.on("click", function (){
+        challengeIndex = Math.floor(Math.random() * hardArr.length)
+        challengeID = hardArr[challengeIndex];
+        $.ajax({
+            url: "https://www.codewars.com/api/v1/code-challenges/" + challengeID,
+            headers: { "Authorization": "MhherAbCWBLgq-YZvg_G" },
+            method: "GET"
+        }).then(function (data) {
+            q.text(data.description)
+            console.log(data)
+        })
     })
 
 });
